3c0e84f6b0eda4effb98914743e6565a9fa5fdad
brunopapait <brunohpapait@gmail.com>
2020-03-26 00:10:15 -0300

    Criando Backend da aplicação ..

---
+++ Backend/src/controllers/OngController.js
@@ -0,0 +1,34 @@
+const connection = require('../database/connection');
+const crypto = require('crypto');
+
+/**
+ * TIPOS DE PARAMETROS
+ * 
+ * QUERY PARAMS: PARAMETROS NOMEDOS NA ROTA APOS O SIMBOLO DE INTERROGACAO, SERVEM PARA FILTROS, PAGINICAO
+ * EX : LOCALHOST:3333/USERS?PAGE=2&IDADE=15&NOME=BRUNO
+ * 
+ * ROUTES PARAMS: UTILIZADOS PARA IDENTIFICAR RECURSOS
+ * EX: LOCALHOST:3333/USERS/1 (CONSULTAR, DELETE BANCO DE DADOS)
+ * BODY: CORPO DA REQUISICAO, UTILIZADO PARA CRIAR OU ALTERAR RECURSOS
+ */
+
+/**
+ * BANCO DE DADOS SQL
+ * 
+ * DRIVER: SELECT * FROM USERS;
+ * QUERY BUILDER: table('users').select('*').where('id' > 15)
+ */
+
+module.exports = {
+    async index(req, res) {
+        const ongs = await connection('ongs').select('*');
+        return res.json(ongs);
+    },
+
+    async create(req, res) {
+        const { name, email, whatsapp, city, uf } = req.body;
+        const id = crypto.randomBytes(4).toString('HEX');
+        await connection('ongs').insert({ id, name, email, whatsapp, city, uf });
+        return res.json({ id });
+    }
+}
