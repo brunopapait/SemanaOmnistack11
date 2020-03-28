44b187fa2e0ea75eecc2a3c95309856bbd9b01f6
brunopapait <brunohpapait@gmail.com>
2020-03-28 01:13:27 -0300

    Implementando aplicacao mobile do projeto ..

---
+++ Mobile/src/pages/Detail/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+export default function Detail () {
+    return <View />;
+}
